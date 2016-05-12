077e827740c50b47b41839b730682576f070db0c
Sage Shaw <sage.b.shaw@gmail.com>
2016-05-12 11:11:55 -0700

    Added command line arguments for setting parameters and a readme explaining them.

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -2,16 +2,21 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 public class Mandelbrot{
-	static double x_center = -.748;
-	static double y_center = .10025;
-	static double x_width = .0002;
-	static int iteration_limit = 2000;
+	//default independent parameters
+	static double x_center = -.25;
+	static double y_center = 0;
+	static double x_width = 4;
+	static int iteration_limit = 50;
 	static int iteration_color_start = 0;
 	static double limit_square = 4;
-	static double res = 8;
-	static int[][] Spectrum = {{100,0,0},{150,0,0},{255,0,255},{0,50,50},{255,200,200}};
-	static int[] set_color = {255,255,255};
+	static double res = .5;
+	static int[][] Spectrum = {{0,0,0},{0,255,0},{200,255,200}};
+	static int[] set_color = {0,0,0};
 
+	//dependent parameters
+	static double y_height, x_min, x_max, y_min, y_max;
+	static int x_pixels, y_pixels;
+	/*
 	static double y_height = x_width * 9.0/16.0;
 	static double x_min = x_center - .5 * x_width;
 	static double x_max = x_center + .5 * x_width;
@@ -19,7 +24,7 @@ public class Mandelbrot{
 	static double y_max = y_center + .5 * y_height;
 	static int x_pixels = (int) Math.round(1920*res);
 	static int y_pixels = (int) Math.round(x_pixels*.5625);
-	
+	*/
 		
 	public static int Converge(Complex z){
 		int i = 0;
@@ -39,6 +44,128 @@ public class Mandelbrot{
 		
 
 	public static void main(String args[]){
+			
+		//Parse args
+/***************************************************************
+	Argument list
+	Mandelbrot [-x xcenter] [-y ycenter] [-w width] [-i iteration-limit iteration-color-start] [-r resolution] [file]
+		-x		the real coordinate of the center of the image
+		-y 		the imaginary coordinate of the center of the image
+		-w 		the width of the viewing window
+		-i		the iteration limit specifies the nubmer of iterations before the point is considered in the set, while the color start indicates the iteration number where the color spectrum begins
+		-r 		the resolution of the image. A resolution of 1 will give a standard 1920x1080 image
+		file	the name of the file. Defaults to "image.png" if not specified
+***************************************************************/
+		String name = "image";
+		int argindex = 0;
+		boolean file_set = false;
+		while(argindex<args.length){
+			switch(args[argindex]){
+				case "-x":
+					if(argindex+1>=args.length){
+						System.err.println("Error: -x flag needs coordinate value.");
+						return;
+					}else{
+						try{
+							x_center = Double.parseDouble(args[argindex+1]);
+						}catch(NumberFormatException e){
+							System.err.println("Error: -x needs a floating point decimal argument.");
+							return;
+						}
+					}
+					argindex += 2;
+					break;
+				case "-y":
+					if(argindex+1>=args.length){
+						System.err.println("Error: -y flag needs coordinate value.");
+						return;
+					}else{
+						try{
+							y_center = Double.parseDouble(args[argindex+1]);
+						}catch(NumberFormatException e){
+							System.err.println("Error: -y needs a floating point decimal argument.");
+							return;
+						}
+					}
+					argindex += 2;
+					break;
+				case "-w":
+					if(argindex+1>=args.length){
+						System.err.println("Error: -w flag needs window width value.");
+						return;
+					}else{
+						try{
+							x_width = Double.parseDouble(args[argindex+1]);
+						}catch(NumberFormatException e){
+							System.err.println("Error: -w needs a floating point decimal argument.");
+							return;
+						}
+					}
+					argindex += 2;
+					break;
+				case "-i":
+					if(argindex+2>=args.length){
+						System.err.println("Error: -i flag needs two integer values.");
+						return;
+					}else{
+						try{
+							iteration_limit = Integer.parseInt(args[argindex+1]);
+							iteration_color_start = Integer.parseInt(args[argindex+2]);
+						}catch(NumberFormatException e){
+							System.err.println("Error: -i needs two integer arguments.");
+							return;
+						}
+					}
+					if(iteration_limit<=iteration_color_start){
+						System.err.println("Error: iteration limit must be larger than the color start.");
+						return;
+					}
+					argindex += 3;
+					break;
+				case "-r":
+					if(argindex+1>=args.length){
+						System.err.println("Error: -r flag needs floating point value.");
+						return;
+					}else{
+						try{
+							res = Double.parseDouble(args[argindex+1]);
+						}catch(NumberFormatException e){
+							System.err.println("Error: -r needs a floating point decimal argument.");
+							return;
+						}
+					}
+					argindex += 2;
+					break;
+				default:
+					if(!file_set){
+						name = args[argindex];
+						file_set = true;
+						argindex++;
+					}else{
+						System.err.println("Error: to many arguments.");
+						return;
+					}
+					break;
+					
+			}
+		}
+		System.out.println("program survived parameter parsing.");
+		System.out.println("-x:\t" + x_center);
+		System.out.println("-y:\t" + y_center);
+		System.out.println("-w:\t" + x_width);
+		System.out.println("-i:\t" + iteration_limit + "   " + iteration_color_start);
+		System.out.println("-r:\t" + res);
+		System.out.println("file:\t" + name);
+		
+		
+		//Adjust program constants
+		y_height = x_width * 9.0/16.0;
+		x_min = x_center - .5 * x_width;
+		x_max = x_center + .5 * x_width;
+		y_min = y_center - .5 * y_height;
+		y_max = y_center + .5 * y_height;
+		x_pixels = (int) Math.round(1920*res);
+		y_pixels = (int) Math.round(x_pixels*.5625);
 		
 		int colors_per_gradient = (iteration_limit - iteration_color_start)/(Spectrum.length-1);
 		//Each gradent includes the lower color, but on the upper number.
@@ -65,7 +192,6 @@ public class Mandelbrot{
 		//the last iteration is set to the set color
 		colors[colors.length-1] = (set_color[0]<<16) + (set_color[1]<<8) + set_color[2];
 		
-		//for(int c=0; c<colors.length; c++){System.out.println(c + ": " + Integer.toHexString(colors[c]));}
 
 		BufferedImage img = new BufferedImage(x_pixels,y_pixels,BufferedImage.TYPE_INT_RGB);
 		
@@ -76,13 +202,12 @@ public class Mandelbrot{
 			for(int y=0; y<y_pixels; y++){
 				int my_count = Converge(PixelsToComplex(x,y))-iteration_color_start;
 				if(my_count<0){my_count = 0;}
-				//System.out.println(my_count);
 				img.setRGB(x,y,colors[my_count]);
 			}
 		}
 		System.out.println("100%");
-		String name;
-		if(args.length>0){name = args[0];}else{name = "image";}
+		//String name;
+		//if(args.length>0){name = args[0];}else{name = "image";}
 		File f = new File(name + ".png");
 		try{
 			ImageIO.write(img, "PNG", f);
---
+++ README.txt
@@ -0,0 +1,23 @@
+This is a java program for generating images of the mandelbrot set.
+
+NAME
+	Mandelbrot - generates images of the mandelbrot set
+	
+SYNOPSIS
+	Mandelbrot [-x xcenter] [-y ycenter] [-w width] 
+		[-i iteration-limit iteration-color-start] [-r resolution] [file]
+
+OPTIONS
+	-x		the real coordinate of the center of the image
+	
+	-y 		the imaginary coordinate of the center of the image
+	
+	-w 		the width of the viewing window
+	
+	-i		the iteration limit specifies the nubmer of iterations before the 
+				point is considered in the set, while the color start indicates 
+				the iteration number where the color spectrum begins
+				
+	-r 		the resolution of the image. A resolution of 1 will give a standard 1920x1080 image
+	
+	file	the name of the file. Defaults to "image.png" if not specified
